b4b770227fac5b36d0c836aebfbd17cbb1e78fe8
Davis-don <davismugoikou@gmail.com>
2024-03-01 12:43:00 -0800

    modified

--- Backend/index.js
+++ Backend/index.js
@@ -6,13 +6,48 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 const cors = require('cors'); 
 app.use(cors());
-
-const nodemailer=require('./nodemailerHandler')
-app.use('api/Send/email',nodemailer)
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 app.get('/',(req,res)=>{
     res.send('server is running')
 })
+app.post('/',(req,res)=>{
+  
+    const {Name,Email,Contact,Message}=req.body
+    const TextMessage=
+    `Name ${Name}
+    Contact ${Contact}
+    Message ${Message}
+    `
+      const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+              user: process.env.UserEmail,
+              pass: process.env.password
+          }
+      });
+      
+      const mailOptions = {
+          from: 'davismadaviaso@gmail.com',
+          to: Email,
+          subject: 'Message from your portfolio app',
+          text: TextMessage
+      };
+      
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              console.error(error);
+          } else {
+            res.status(200).json({message:'email sent successfully'})
+              console.log('Email sent: ' + info.response);
+          }
+      });
+         
+    })
+
+
+
 app.listen(4000,(error)=>{
     if(error) throw error
     console.log('listening to port 4000')
