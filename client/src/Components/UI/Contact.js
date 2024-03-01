f33317b5c8fd2e05ec27d723903737c10b729593
Davis-don <davismugoikou@gmail.com>
2024-03-01 11:38:35 -0800

    changed location of vercel.json

--- client/src/Components/UI/Contact.js
+++ client/src/Components/UI/Contact.js
@@ -22,7 +22,7 @@ const [emailData,setEmailData]=useState({
     let handlepost = async (e)=>{
         e.preventDefault();
 try{
-const response= await fetch ('/',{
+const response= await fetch ('api/Send/email',{
     method:'post',
     headers:{
         "content-type":'application/json'
