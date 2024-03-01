0b7e34ba54a6ed3164e947dba03be0c831d84165
Davis-don <davismugoikou@gmail.com>
2024-03-01 12:26:09 -0800

    modified server side

--- Backend/index.js
+++ Backend/index.js
@@ -10,9 +10,9 @@ app.use(cors());
 const nodemailer=require('./nodemailerHandler')
 app.use('api/Send/email',nodemailer)
 
-app.use('/',(req,res)=>{
-    res.send('server is running')
-})
+// app.use('/',(req,res)=>{
+//     res.send('server is running')
+// })
 app.listen(4000,(error)=>{
     if(error) throw error
     console.log('listening to port 4000')
