2c8ba31f2c7af296cae674f13b51bd344c86168e
Davis-don <davismugoikou@gmail.com>
2024-03-03 12:40:54 -0800

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -22,36 +22,36 @@ app.get('/',(req,res)=>{
 })
 app.post('/',(req,res)=>{
   
-    //const {Name,Email,Contact,Message}=req.body
+    const {Name,Email,Contact,Message}=req.body
     res.status(200).json({message:'email sent successfully'})
-    // const TextMessage=
-    // `Name ${Name}
-    // Contact ${Contact}
-    // Message ${Message}
-    // `
-    //   const transporter = nodemailer.createTransport({
-    //       service: 'gmail',
-    //       auth: {
-    //           user: process.env.UserEmail,
-    //           pass: process.env.password
-    //       }
-    //   });
+    const TextMessage=
+    `Name ${Name}
+    Contact ${Contact}
+    Message ${Message}
+    `
+      const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+              user: process.env.UserEmail,
+              pass: process.env.password
+          }
+      });
       
-    //   const mailOptions = {
-    //       from: 'davismadaviaso@gmail.com',
-    //       to: Email,
-    //       subject: 'Message from your portfolio app',
-    //       text: TextMessage
-    //   };
+      const mailOptions = {
+          from: 'davismadaviaso@gmail.com',
+          to: Email,
+          subject: 'Message from your portfolio app',
+          text: TextMessage
+      };
       
-    //   transporter.sendMail(mailOptions, (error, info) => {
-    //       if (error) {
-    //           console.error(error);
-    //       } else {
-    //         res.status(200).json({message:'email sent successfully'})
-    //           console.log('Email sent: ' + info.response);
-    //       }
-    //   });
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              console.error(error);
+          } else {
+            res.status(200).json({message:'email sent successfully'})
+              console.log('Email sent: ' + info.response);
+          }
+      });
          
     })
 
