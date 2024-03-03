cab0636d65070a8a6df3a30b9cfddaae6d012231
Davis-don <davismugoikou@gmail.com>
2024-03-03 12:45:31 -0800

    cors

--- client/src/Components/UI/Contact.js
+++ client/src/Components/UI/Contact.js
@@ -22,7 +22,7 @@ const [emailData,setEmailData]=useState({
     let handlepost = async (e)=>{
         e.preventDefault();
 try{
-const response= await fetch ('http://trevordunahappserver.vercel.app',{
+const response= await fetch ('https://trevordunahappserver.vercel.app',{
     method:'post',
     headers:{
         "content-type":'application/json'
